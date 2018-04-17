fc6bc1c30586781be10bb588b2f56a1718128ceb
nujhong <w.chen-2@hotmail.com>
2018-04-17 17:52:37 +1000

    First commit

---
+++ src/layouts/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import './all.sass'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <div>{children()}</div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
