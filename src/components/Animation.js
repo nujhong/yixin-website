1b0970998f1417de805b582fedf27e91c33e4f37
nujhong <w.chen-2@hotmail.com>
2018-04-19 18:11:18 +1000

    Disable wowjs

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import 'animate.css/animate.css';
 
-if (typeof window !== `undefined`) {
-  const WOW = require('wowjs');
-}
+// if (typeof window !== `undefined`) {
+//   const WOW = require('wowjs');
+// }
 
 class Animation extends Component {
 
   componentDidMount() {
-    new WOW.WOW({
-        live: false
-    }).init()
+    // new WOW.WOW({
+    //     live: false
+    // }).init()
   }
 
   render() {
