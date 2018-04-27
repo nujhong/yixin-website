106083b40651145d7cf70bd3ddad09ba08d233e3
nujhong <w.chen-2@hotmail.com>
2018-04-27 16:44:52 +1000

    Restructured data schema

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+const _ = require('lodash')
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+	const { createNodeField } = boundActionCreators
+
+	if (node.internal.type === `MarkdownRemark`) {
+		const value = createFilePath({ node, getNode })
+		createNodeField({
+			name: `slug`,
+			node,
+			value,
+		})
+	}
+}
